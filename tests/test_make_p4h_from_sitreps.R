c569592ee29ac50b2a0157198a301b4c2630589a
HorridTom <woodcock.thomas@gmail.com>
2018-05-16 10:40:50 +0100

    Added test for dimensions and colnames of 4 hour performance dataframe

--- tests/test_make_p4h_from_sitreps.R
+++ tests/test_make_p4h_from_sitreps.R
@@ -2,8 +2,22 @@ source('../perf_4h_analysis.R')
 context("Convert downloaded sitrep data to tidy data")
 
 load('test-data/AE_data_test.rda')
+load('test-data/perf_data_colnames.rda')
 
 testthat::test_that("Output of conversion is a tibble",{
     p4h <- make_p4h_from_sitreps(AE_data_test)
+    
     testthat::expect_is(p4h, 'tbl')
 })
+
+testthat::test_that("Output of conversion has correct dimensions and colnames",{
+    p4h <- make_p4h_from_sitreps(AE_data_test)
+
+    testthat::expect_equal(colnames(p4h), perf_data_colnames)
+    testthat::expect_equal(nrow(p4h), 192)
+})
+
+# Should rapidly refactor to remove need to go to this data format if possible.
+#testthat::test_that("Some example org-months are correctly converted",{
+#    p4h <- make_p4h_from_sitreps(AE_data_test)
+#})
