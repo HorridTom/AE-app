38acb514aa07836c9703f28e8bd7f919651ff10e
Imogen Connor-Helleur <imooconnor@outlook.com>
2018-09-10 23:28:57 +0100

    Fixed bug with attendances plot scales.

--- perf_4h_analysis.R
+++ perf_4h_analysis.R
@@ -279,9 +279,11 @@ plot_volume <- function(df, code = "RBZ", date.col = 'Month_Start',
                    limits = c(q.st.dt, q.ed.dt)) +
       ggtitle(cht_title, subtitle = paste(levelTitle,pr_name, typeTitle)) + 
       labs(x= x_title, y="Number of attendances",
-           caption = "*Shewhart chart rules apply (see Understanding the Analysis tab for more detail) \nRule 1: Any month outside the control limits \nRule 2: Eight or more consecutive months all above, or all below, the centre line", size = 10) #+
-      #scale_y_continuous(limits = c(ylimlow, ylimhigh),
-      #                   breaks = seq(ylimlow, ylimhigh, 1000*round((ylimhigh-ylimlow)/8/1000))) 
+           caption = "*Shewhart chart rules apply (see Understanding the Analysis tab for more detail) \nRule 1: Any month outside the control limits \nRule 2: Eight or more consecutive months all above, or all below, the centre line",
+           size = 10) +
+      scale_y_continuous(limits = c(ylimlow, ylimhigh),
+                         breaks = seq(ylimlow, ylimhigh, 1000*(ylimhigh-ylimlow)/8/1000)
+                         ) 
     
   } else {df}
 }
